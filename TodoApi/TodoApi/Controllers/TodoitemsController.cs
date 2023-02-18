ec922d24b67385069bad89578d930535f839d684
iSleepyP <supapol9993@hotmail.co.th>
2023-02-19 01:31:54 +0700

    example

---
+++ TodoApi/TodoApi/Controllers/TodoitemsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/Todoitems")]
+    [ApiController]
+    public class TodoitemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoitemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Todoitems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todoitem>>> GetTodoItems()
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/Todoitems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todoitem>> GetTodoitem(long id)
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            var todoitem = await _context.TodoItems.FindAsync(id);
+
+            if (todoitem == null)
+            {
+                return NotFound();
+            }
+
+            return todoitem;
+        }
+
+        // PUT: api/Todoitems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoitem(long id, Todoitem todoitem)
+        {
+            if (id != todoitem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoitem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoitemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Todoitems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Todoitem>> PostTodoitem(Todoitem todoitem)
+        {
+            _context.TodoItems.Add(todoitem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoitem", new { id = todoitem.Id }, todoitem);
+        }
+
+        // DELETE: api/Todoitems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoitem(long id)
+        {
+            if (_context.TodoItems == null)
+            {
+                return NotFound();
+            }
+            var todoitem = await _context.TodoItems.FindAsync(id);
+            if (todoitem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoitem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoitemExists(long id)
+        {
+            return (_context.TodoItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ TodoApi/TodoApi/Models/Todoitem.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models
+{
+    public class Todoitem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Iscomplete { get; set; }
+
+    }
+}
